c0c56a096657e470342ed9326a0236b7d2faec56
WalterWangNYUMLC <mr.walterwang@gmail.com>
2019-06-16 20:03:25 -0400

    added att

--- board.py
+++ board.py
@@ -1,13 +1,17 @@
 class Board:
     def __init__(self):
         self.units = {}
-        "objects to be updated"
         self.pop_count = 0
 
-    def add(self, unit):
-        if unit.pop + self.pop_count > 10:
-            return 'pop limit reached'
+    def add(self, unit, x, y):
+        try:
+            self.check_add(unit, x, y)
+        except Exception as e:
+            print('Failed to add {}, error {}'.format(unit, e))
+            return
         self.pop_count += unit.pop
+        unit.pos = [x, y]
+        unit.orient = [1, 1]
         unit.uid = self._get_uid()
         self.units[unit.uid] = unit
         self.update()
@@ -15,14 +19,28 @@ class Board:
     def remove(self, uid):
         self.pop_count -= self.units[uid].pop
         self.units.pop(uid)
+        del self.units[uid].pos
+        del self.units[uid].orient
         self.update()            
+
+    def check_add(self, unit, x, y):
+        if unit.pop + self.pop_count > 10:
+            raise Exception('pop limit reached')
+
+        for u in self.units.values():
+            if u.pos == [x, y]:
+                raise Exception('{}, {} is occupied by {}'.format(
+                    x, y, u))
 
     def update(self):
         for u in self.units.values():
             u.update_interactions(list(self.units.values()))
 
-    def register(self):
-        pass
+    def reverse_board(self):
+        " for player 2, y is flipped and orientation is flipped"
+        for u in self.units.values():
+            u.pos = [u.pos[0], -u.pos[1]]
+            u.orient = [u.orient[0], -u.orient[1]]
 
     def _get_uid(self):
         for i in range(1,100):
